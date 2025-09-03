7be489e93f75bd389ce695414e6b0426ce51b0b9
Aleksei <bylexatime@gmail.com>
2025-09-03 23:48:54 +0300

    check Seo category

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,24 @@
-import css from './page.module.css';
 import type { Metadata } from 'next';
+import css from './page.module.css';
 
-interface Props {
-  params: { slug?: string[] };
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  return {
+export const metadata: Metadata = {
+  title: '404 - Page Not Found | NoteHUB',
+  description: 'The page you are looking for does not exist on NoteHUB.',
+  openGraph: {
     title: '404 - Page Not Found | NoteHUB',
-    description: 'The page you are looking for does not exist on NoteHUB.',
-    openGraph: {
-      title: '404 - Page Not Found | NoteHUB',
-      description: 'Oops! The page you are trying to access does not exist.',
-      url: 'https://08-zustand-blush-one.vercel.app/not-found',
-      images: [
-        {
-          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-          width: 1200,
-          height: 600,
-          alt: '404 - Page Not Found',
-        },
-      ],
-      type: 'website',
-    },
-  };
-}
+    description: 'Oops! The page you are trying to access does not exist.',
+    url: 'https://08-zustand-blush-one.vercel.app/not-found',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 600,
+        alt: '404 - Page Not Found',
+      },
+    ],
+    type: 'website',
+  },
+};
 
 export default function NotFound() {
   return (
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -6,13 +6,38 @@ import {
 } from '@tanstack/react-query';
 import NotesClient from './Notes.client';
 import { NoteTag } from '@/types/note';
+import { Metadata } from 'next';
 
 type Props = {
-  params: Promise<{ slug: string[] }>;
+  params: { slug: string[] };
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = params.slug;
+  const filter = slug?.[0] || 'All';
+
+  return {
+    title: `Notes filtered by "${filter}" | NoteHUB`,
+    description: `Browse your notes filtered by tag "${filter}" in NoteHUB.`,
+    openGraph: {
+      title: `Notes filtered by "${filter}" | NoteHUB`,
+      description: `Easily explore notes that are tagged with "${filter}" in NoteHUB.`,
+      url: `https://08-zustand-blush-one.vercel.app/notes/filter/${filter}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 600,
+          alt: `Notes filtered by ${filter}`,
+        },
+      ],
+      type: 'website',
+    },
+  };
+}
+
 const NotesPage = async ({ params }: Props) => {
-  const { slug } = await params;
+  const { slug } = params;
   const tag = slug[0] === 'All' ? undefined : (slug[0] as NoteTag);
   const queryClient = new QueryClient();
 
