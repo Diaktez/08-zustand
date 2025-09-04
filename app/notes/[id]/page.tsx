d8fce4d49cee92237bd668d89e0b0c4e61a5a74d
Aleksei <bylexatime@gmail.com>
2025-09-04 19:48:26 +0300

    check detailed note

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -10,6 +10,35 @@ type Props = {
   params: Promise<{ id: string }>;
 };
 
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+
+  const url = `https://08-zustand-eight-phi.vercel.app/notes/${id}`;
+
+  const description = note.content.slice(0, 100);
+
+  return {
+    title: `Note: ${note.title}`,
+    description,
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description,
+      siteName: 'NoteHub',
+      url,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 600,
+          alt: `Note: ${note.title}`,
+        },
+      ],
+      type: 'website',
+    },
+  };
+}
+
 export default async function NoteDetailsPage({ params }: Props) {
   const { id } = await params;
   const queryClient = new QueryClient();
